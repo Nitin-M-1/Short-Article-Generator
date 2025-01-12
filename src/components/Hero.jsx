0c45307ca6d3310383057ed7486892d9475323dd
Nitin-M-1 <condyman1234@gmail.com>
2025-01-12 19:34:24 +0530

    fix

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -11,7 +11,7 @@ const Hero = () => {
         <button
           type='button'
           onClick={() =>
-            window.open("https://github.com/TidbitsJS/Summize", "_blank")
+            window.open("https://github.com/Nitin-M-1/Short-Article-Summaries-Generator", "_blank")
           }
           className='black_btn'
         >
